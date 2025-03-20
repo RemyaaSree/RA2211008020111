4136a691cefa0e1176524ecaaf888061c934de63
RemyaaSree <128922487+RemyaaSree@users.noreply.github.com>
2025-03-20 19:55:11 +0530

    Create helpers.ts

---
+++ Develop a React based Social Media Analytics Frontend Web Application/src/utils/helpers.ts
@@ -0,0 +1,32 @@
+import { User } from '../types';
+
+export const getUserName = (users: User[] | undefined, userId: number): string => {
+  return users?.find(user => user.id === userId.toString())?.name || 'Unknown User';
+};
+
+export const formatDate = (date: Date): string => {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(date);
+};
+
+export const debounce = <T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): ((...args: Parameters<T>) => void) => {
+  let timeout: NodeJS.Timeout;
+
+  return function executedFunction(...args: Parameters<T>) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}; 
